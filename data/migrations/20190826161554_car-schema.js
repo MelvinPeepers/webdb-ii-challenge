a91dc4c24d142e7ada3c384de57058d6d03d322f
MelvinPeepers <peepersm@gmail.com>
2019-08-26 16:45:08 -0400

    configured schema

--- data/migrations/20190826161554_car-schema.js
+++ data/migrations/20190826161554_car-schema.js
@@ -1,8 +1,19 @@
-
+// the changes we want to make
 exports.up = function(knex) {
-  
+  // create a car table
+  // define the schema
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments();
+    tbl.string('make', 36).notNullable();
+    tbl.string('modle', 36).notNullable();
+    tbl.integer('mileage', 999999).notNullable();;
+    tbl.string('transmission type' 36);
+    tbl.string('title', 36);
+  });
 };
 
+// undoes the changes
 exports.down = function(knex) {
-  
+  // drop the fruits table
+  return knex.schema.dropTableIfExists('cars');
 };
